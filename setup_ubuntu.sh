71c1b25a100fef1420c4249ceba5b379ef9d89c1
NotDev <93055930+EikeiDev@users.noreply.github.com>
2023-12-29 00:38:43 +0400

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -6,12 +6,15 @@ apt update && apt upgrade -y
 # 2) Установка UFW (Uncomplicated Firewall)
 apt install ufw -y
 
-# 3) Создание нового пользователя и добавление его в группу sudo
+# 3) Установка bc
+apt-get install bc -y
+
+# 4) Создание нового пользователя и добавление его в группу sudo
 read -p "Введите имя нового пользователя: " new_user
 adduser $new_user
 usermod -aG sudo $new_user
 
-# 4) Генерация ключей SSH для нового пользователя и настройка авторизации по ключам
+# 5) Генерация ключей SSH для нового пользователя и настройка авторизации по ключам
 mkdir -p /home/$new_user/.ssh
 chmod 700 /home/$new_user/.ssh
 ssh-keygen -t rsa -b 2048 -f /home/$new_user/.ssh/id_rsa -N ""
@@ -19,7 +22,7 @@ cat /home/$new_user/.ssh/id_rsa.pub >> /home/$new_user/.ssh/authorized_keys
 chmod 600 /home/$new_user/.ssh/authorized_keys
 chown -R $new_user:$new_user /home/$new_user/.ssh
 
-# 5) Изменение порта SSH, настройка безопасности и настройки аутентификации
+# 6) Изменение порта SSH, настройка безопасности и настройки аутентификации
 os_version=$(lsb_release -sr)
 if (( $(echo "$os_version >= 23.04" | bc -l) )); then
     read -p "Введите новый порт SSH для Ubuntu 23.04 и выше: " sshport
@@ -44,19 +47,17 @@ sed -i '/^PermitRootLogin/ c\PermitRootLogin no' /etc/ssh/sshd_config
 # Создание файла /etc/ssh/sshd_config.d/50-cloud-init.conf и добавление параметра PasswordAuthentication
 echo "PasswordAuthentication no" > /etc/ssh/sshd_config.d/50-cloud-init.conf
 
-# 6) Добавление нового порта в файерволл UFW и активация UFW
+# 7) Добавление нового порта в файерволл UFW и активация UFW
 ufw allow $sshport/tcp
 ufw enable
 
-# 7) Отключение учетной записи root
+# 8) Отключение учетной записи root
 sed -i 's|^root:x:.*:/bin/bash|root:x:0:0:root:/root:/usr/sbin/nologin|' /etc/passwd
 
-# 8) Перезапуск сервиса SSH для применения изменений
+# 9) Перезапуск сервиса SSH для применения изменений
 systemctl restart ssh
 
-# 9) Вывод приватного ключа нового пользователя
+# 10) Вывод приватного ключа нового пользователя
 echo "Приватный ключ нового пользователя:"
 cat /home/$new_user/.ssh/id_rsa
-
-# Вывод сообщения о завершении настройки
 echo -e "\nНастройка завершена."
