fc2840e3c478e46c738145790ab6a519ac82462d
NotDev <93055930+EikeiDev@users.noreply.github.com>
2023-12-28 22:50:07 +0400

    Обновил логику скрипта

--- setup_debian.sh
+++ setup_debian.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Обновление системы
-apt update
-apt upgrade -y
+# 1) Обновление системы
+apt update && apt upgrade -y
 
-# Установка UFW
-apt install -y ufw
+# 2) Установка UFW (Uncomplicated Firewall)
+apt install ufw -y
 
-# 1. Создание нового пользователя и добавление в группу sudo
+# 3) Создание нового пользователя и добавление его в группу sudo
 read -p "Введите имя нового пользователя: " new_user
 adduser $new_user
 usermod -aG sudo $new_user
 
-# 2. Генерация ключей и авторизация по ключам
+# 4) Генерация ключей SSH для нового пользователя и настройка авторизации по ключам
 mkdir -p /home/$new_user/.ssh
 chmod 700 /home/$new_user/.ssh
 ssh-keygen -t rsa -b 2048 -f /home/$new_user/.ssh/id_rsa -N ""
@@ -20,24 +19,28 @@ cat /home/$new_user/.ssh/id_rsa.pub >> /home/$new_user/.ssh/authorized_keys
 chmod 600 /home/$new_user/.ssh/authorized_keys
 chown -R $new_user:$new_user /home/$new_user/.ssh
 
-# 3. Изменение порта SSH и настройка безопасности
-read -p "Введите новый порт для SSH: " new_ssh_port
-sed -i "s/^#Port 22/Port $new_ssh_port/" /etc/ssh/sshd_config
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+# 5) Изменение порта SSH, настройка безопасности и запрет аутентификации по паролю
+read -p "Введите новый порт SSH: " sshport
+sed -i "s/#Port 22/Port $sshport/" /etc/ssh/sshd_config
 
-# 4. Добавление нового порта в файерволл UFW
-ufw allow $new_ssh_port
+# Активация изменений параметров PasswordAuthentication и PermitRootLogin
+sed -i '/^PasswordAuthentication/ c\PasswordAuthentication no' /etc/ssh/sshd_config
+sed -i '/^PermitRootLogin/ c\PermitRootLogin no' /etc/ssh/sshd_config
+
+# Создание файла /etc/ssh/sshd_config.d/50-cloud-init.conf и добавление параметра PasswordAuthentication
+echo "PasswordAuthentication no" > /etc/ssh/sshd_config.d/50-cloud-init.conf
+
+# 6) Добавление нового порта в файерволл UFW и активация UFW
+ufw allow $sshport/tcp
 ufw enable
 
-# 5. Отключение учетной записи root
+# 7) Отключение учетной записи root
 sed -i 's|^root:x:.*:/bin/bash|root:x:0:0:root:/root:/usr/sbin/nologin|' /etc/passwd
 
-# Перезапуск сервиса SSH для применения изменений
-systemctl ssh restart
-systemctl sshd restart
+# 8) Перезапуск сервиса SSH для применения изменений
+systemctl restart ssh
 
-# Вывод приватного ключа
-echo "Ваш приватный ключ для подключения к серверу:"
+# 9) Вывод приватного ключа нового пользователя
+echo "Приватный ключ нового пользователя:"
 cat /home/$new_user/.ssh/id_rsa
 echo -e "\nНастройка завершена."
